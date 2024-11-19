c189285ae7eabae94535caf7f5a72dc0340ae8f6
juhgadotti <jgadotti27@gmail.com>
2024-11-18 23:49:35 -0300

    TA FUNCIONNDO

--- Food2Desk/src/app/components/user-home/user-home.component.ts
+++ Food2Desk/src/app/components/user-home/user-home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-home',
+  imports: [CommonModule],
   templateUrl: './user-home.component.html',
-  styleUrls: ['./user-home.component.scss']
+  styleUrls: ['./user-home.component.scss'],
+  standalone: true
 })
 export class UserHomeComponent {
+  items = [
+    { name: 'Item 1', description: 'Description of item 1', price: 10.99 },
+    { name: 'Item 2', description: 'Description of item 2', price: 20.49 },
+    { name: 'Item 3', description: 'Description of item 3', price: 15.75 },
+  ];
   
 }
