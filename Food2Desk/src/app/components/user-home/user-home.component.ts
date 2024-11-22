e1015a6b97a621b147b4a9a37589260c5f5405b8
juhgadotti <jgadotti27@gmail.com>
2024-11-21 22:14:00 -0300

    commit

--- Food2Desk/src/app/components/user-home/user-home.component.ts
+++ Food2Desk/src/app/components/user-home/user-home.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   selector: 'app-user-home',
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './user-home.component.html',
   styleUrls: ['./user-home.component.scss'],
   standalone: true
 })
 export class UserHomeComponent {
   items = [
-    { name: 'Item 1', description: 'Description of item 1', price: 10.99 },
+    { image: '../../img/food/coca.png', name: 'Item 1', description: 'Description of item 1', price: 10.99 },
     { name: 'Item 2', description: 'Description of item 2', price: 20.49 },
     { name: 'Item 3', description: 'Description of item 3', price: 15.75 },
   ];
