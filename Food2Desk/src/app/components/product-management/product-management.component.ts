1c0430c14f1658a9848a3451339fdc61226424c2
juhgadotti <jgadotti27@gmail.com>
2025-02-12 20:32:06 -0300

    criando arquivo de product management

---
+++ Food2Desk/src/app/components/product-management/product-management.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-management',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './product-management.component.html',
+  styleUrl: './product-management.component.scss'
+})
+export class ProductManagementComponent {
+
+}
