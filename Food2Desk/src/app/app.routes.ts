d10a6f7e468df4d8fe42c4aaf0c1442049bd99d0
juhgadotti <jgadotti27@gmail.com>
2025-02-12 21:14:16 -0300

    começando o forms de cadastro de produto

--- Food2Desk/src/app/app.routes.ts
+++ Food2Desk/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { UserHomeComponent } from './components/user-home/user-home.component';
 import { UserOrderStatusComponent } from './components/user-order-status/user-order-status.component';
 import { AdminManageComponent } from './components/admin-manage/admin-manage.component';
 import { SavingAppComponent } from './components/saving-app/saving-app.component';
+import { ProductManagementComponent } from './components/product-management/product-management.component';
 
 
 export const routes: Routes = [
     {path: "user-home", component: UserHomeComponent},
     {path: "user-order-status", component: UserOrderStatusComponent},
     {path: "saving-app", component: SavingAppComponent},
-    {path: "admin-manage", component: AdminManageComponent}
+    {path: "admin-manage", component: AdminManageComponent},
+    {path: "product-management", component: ProductManagementComponent}
 ];
