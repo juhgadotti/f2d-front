edf9b5b7044202873405f1a499c946abe50986ca
juhgadotti <jgadotti27@gmail.com>
2025-03-19 22:54:47 -0300

    fix: order & user interface to match backend object

--- Food2Desk/src/app/interfaces/user.ts
+++ Food2Desk/src/app/interfaces/user.ts
@@ -1,5 +1,5 @@
 export interface User {
     id: string
     name: string
-    offices: [{officeId: string, floor: string, number: string, block: string | null, enterprise: string | null}]
+    offices: [{officeId: string, floor: string, number: string, block: string | null, enterprise: string | null, enterpriseId: string | null, obs: string | null}]
 }
