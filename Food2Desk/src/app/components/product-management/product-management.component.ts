e7c48409638707c9e67bd4e64ffdf12ab1a09c8d
juhgadotti <jgadotti27@gmail.com>
2025-03-30 20:58:34 -0300

    add: product register in product management

--- Food2Desk/src/app/components/product-management/product-management.component.ts
+++ Food2Desk/src/app/components/product-management/product-management.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Product } from '../../interfaces/product';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Food2DeskApi } from '../../../environments/path';
+
 
 @Component({
   selector: 'app-product-management',
@@ -10,19 +14,33 @@ import { Product } from '../../interfaces/product';
   styleUrl: './product-management.component.scss'
 })
 
-export class ProductManagementComponent {
-  name!: string | null;
-  description!: string | null;
-  category: string = '';
-  price!: number | null;
+export class ProductManagementComponent implements OnInit {
+  constructor(private router: Router, private http: HttpClient) {}
 
+  name!: string;
+  description!: string;
+  category: string = '';
+  price!: number;
 
   product: Product = {} as Product;
 
-  saveProduct(product: any){
-    product.name = this.name;
-    product.price = this.price;
+  private urls = Food2DeskApi.urls;
 
-    console.log(product)
+ // options = ['Bebida', 'Salgado', 'Doce', 'Almoço']
+  
+  ngOnInit(){
+   
+  }
+
+  saveProduct(){
+    this.product.name = this.name;
+    this.product.price = this.price;
+    this.product.description = this.description;    
+
+    console.log(this.product)
+
+    this.http.post<Product>(this.urls.product.root, this.product).subscribe(response => {
+          console.log(response);
+        });
   }
 }
