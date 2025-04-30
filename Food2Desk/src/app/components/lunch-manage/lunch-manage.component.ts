613d8a2c4590d59557372daa53cc3d217c8cce3b
juhgadotti <jgadotti27@gmail.com>
2025-04-30 16:04:00 -0300

    generate lunch manage component

---
+++ Food2Desk/src/app/components/lunch-manage/lunch-manage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lunch-manage',
+  standalone: true,
+  imports: [],
+  templateUrl: './lunch-manage.component.html',
+  styleUrl: './lunch-manage.component.scss'
+})
+export class LunchManageComponent {
+
+}
