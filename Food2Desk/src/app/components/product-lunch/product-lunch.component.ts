df0c2213989466e91847cf9d31b4c46d4b232e0d
juhgadotti <jgadotti27@gmail.com>
2025-05-01 17:50:25 -0300

    product lunch inicial botoes edit mode

--- Food2Desk/src/app/components/product-lunch/product-lunch.component.ts
+++ Food2Desk/src/app/components/product-lunch/product-lunch.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-lunch',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-lunch.component.html',
   styleUrl: './product-lunch.component.scss'
 })
-export class ProductLunchComponent {
 
+export class ProductLunchComponent {
+  weekDays = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado', 'Domingo'];
+
+  lunchMenu: { [key: string]: { name: string; quantity: number }[] } = {
+    'Segunda': [{ name: 'Parmegiana de Frango', quantity: 20 }, { name: 'Peixe', quantity: 15 }],
+    'Terça': [{ name: 'Feijoada', quantity: 30 }, { name: 'Bife Acebolado', quantity: 15 }],
+    'Quarta': [],
+    'Quinta': [],
+    'Sexta': [],
+    'Sabado': [],
+    'Domingo': [],
+  };
+
+  editMode: { [key: string]: boolean } = {};
+
+  addDish(day: string) {
+    const dishName = prompt(`Digite o nome do prato para ${day}:`);
+    const quantity = prompt(`Digite a quantidade de porções para ${dishName}:`);
+
+    if (dishName && quantity && !isNaN(Number(quantity))) {
+      this.lunchMenu[day].push({ name: dishName, quantity: Number(quantity) });
+    }
+  }
+
+  toggleEditMode(day: string) {
+    this.editMode[day] = !this.editMode[day];
+  }
+
+  removeDish(day: string, index: number) {
+    this.lunchMenu[day].splice(index, 1);
+  }
 }
