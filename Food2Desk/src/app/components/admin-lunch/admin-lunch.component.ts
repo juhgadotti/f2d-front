6d0074e395eb9bbf764bf56b5c071e57f7f358fc
juhgadotti <jgadotti27@gmail.com>
2025-05-06 23:43:16 -0300

    header no admin lunch

--- Food2Desk/src/app/components/admin-lunch/admin-lunch.component.ts
+++ Food2Desk/src/app/components/admin-lunch/admin-lunch.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   selector: 'app-admin-lunch',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './admin-lunch.component.html',
   styleUrl: './admin-lunch.component.scss'
 })
