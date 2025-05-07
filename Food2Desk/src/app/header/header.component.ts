f7aa7c75470bca600be576a49c5c9a5bd9198a3e
juhgadotti <jgadotti27@gmail.com>
2025-05-06 23:35:16 -0300

    header ta drin do drin falta só o gerenciar almoço

--- Food2Desk/src/app/header/header.component.ts
+++ Food2Desk/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,40 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
-isUser: boolean = true;
+  constructor(private router: Router) {
+
+  }
+  isUser: boolean = true;
 
   ngOnInit(): void {
-    
+    //pegar tipo de usuario 
+  }
+
+  redirectUser(url: number) {
+    switch (url) {
+      case 1:
+        this.router.navigate(['/user-home'])
+        break;
+      case 2:
+        this.router.navigate(['/user-order-status'])
+        break;
+      case 3:
+        this.router.navigate(['/profile'])
+        break;
+    }
+  }
+
+  redirectAdmin(url: number) {
+    switch (url) {
+      case 1:
+        this.router.navigate(['/admin-manage'])
+        break;
+      case 2:
+        this.router.navigate(['/user-order-status'])
+        break;
+      case 3:
+        this.router.navigate(['/product-management'])
+        break;
+    }
   }
 }
