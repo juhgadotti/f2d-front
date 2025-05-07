96bedb7de7dc2b13c4813a714825666c6eebc891
juhgadotti <jgadotti27@gmail.com>
2025-05-06 23:41:51 -0300

    pagina de gerenciar pedidos de almoço

---
+++ Food2Desk/src/app/components/admin-lunch/admin-lunch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-lunch',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-lunch.component.html',
+  styleUrl: './admin-lunch.component.scss'
+})
+export class AdminLunchComponent {
+
+}
