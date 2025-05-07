1999a7762f04ffc2869d7e595d4925476b9ff197
juhgadotti <jgadotti27@gmail.com>
2025-05-07 14:16:54 -0300

    fazendo a tela de admin-home

--- Food2Desk/src/app/components/admin-home/admin-home.component.ts
+++ Food2Desk/src/app/components/admin-home/admin-home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule, RouterModule],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.scss'
 })
 export class AdminHomeComponent {
-
+  constructor(private router: Router){}
 }
