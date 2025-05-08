e4eddabab73bee241488124a369d828a374779fb
juhgadotti <jgadotti27@gmail.com>
2025-05-08 14:03:55 -0300

    fazendo screen kitchen

--- Food2Desk/src/app/components/screen-kitchen/screen-kitchen.component.ts
+++ Food2Desk/src/app/components/screen-kitchen/screen-kitchen.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../../header/header.component';
+import { Order } from '../user-home/order';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Food2DeskApi } from '../../../environments/path';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-screen-kitchen',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './screen-kitchen.component.html',
   styleUrl: './screen-kitchen.component.scss'
 })
-export class ScreenKitchenComponent {
+export class ScreenKitchenComponent implements OnInit {
+  constructor(private router: Router, private http: HttpClient) { }
 
+  private urls = Food2DeskApi.urls;
+  orderList: Order[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Order[]>(this.urls.order.root).subscribe(response => {
+      console.log(response)
+      this.orderList = response.filter(p => p.status == 1);
+    });
+  }
 }
