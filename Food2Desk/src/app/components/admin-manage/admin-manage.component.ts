46aed597474aae68abd76a2b3fe288e5405e3ef4
juhgadotti <jgadotti27@gmail.com>
2025-05-14 18:35:56 -0300

    arrumando o tipo de order pra ter o showDetails (a função de filtro não estava funcionando)

--- Food2Desk/src/app/components/admin-manage/admin-manage.component.ts
+++ Food2Desk/src/app/components/admin-manage/admin-manage.component.ts
@@ -15,23 +15,25 @@ import { Router } from '@angular/router';
   styleUrl: './admin-manage.component.scss'
 })
 
+
 export class AdminManageComponent implements OnInit {
-  constructor(private router: Router, private http: HttpClient) {}
-    
-    private urls = Food2DeskApi.urls;
-    newOrderList: Order[] = [];
-    showThirdColumn: boolean = false;
-    showDetails: boolean = false;
+  constructor(private router: Router, private http: HttpClient) { }
+
+  private urls = Food2DeskApi.urls;
+  //newOrderList: Order[] = [];
+  showThirdColumn: boolean = false;
+  //showDetails: boolean = false;socorro
+  newOrderList: (Order & { showDetails?: boolean })[] = [];
 
   ngOnInit(): void {
-    this.http.get<Order[]>(this.urls.order.root).subscribe(response =>{
+    this.http.get<Order[]>(this.urls.order.root).subscribe(response => {
       console.log(response)
-      this.newOrderList = response;
-    });      
-  } 
+      this.newOrderList = response.map(item => ({ ...item, showDetails: false }));
+    });
+  }
 
-  updateOrderStatus(order: Order, newStatus: number) {        
-    this.newOrderList = this.newOrderList.map(item => item.id == order.id ? {...item, status: newStatus} : item);
+  updateOrderStatus(order: Order, newStatus: number) {
+    this.newOrderList = this.newOrderList.map(item => item.id == order.id ? { ...item, status: newStatus } : item);
 
     //const params = new HttpParams().set('id', order.id.toString());
     order.status = newStatus;
@@ -41,7 +43,8 @@ export class AdminManageComponent implements OnInit {
     //att a lista
   }
 
-  orderListStatus(status: number): Order[]{
-    return this.newOrderList.filter(p => p.status == status)
+  orderListStatus(status: number): (Order & { showDetails?: boolean })[] {
+    return this.newOrderList.filter(p => p.status == status);
   }
+
 }
