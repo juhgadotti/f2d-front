bbc2b50764742021c532c985af975a7ce04fad2e
juhgadotti <jgadotti27@gmail.com>
2025-05-17 10:11:57 -0300

    order lunch

--- Food2Desk/src/app/components/order-lunch/order-lunch.component.ts
+++ Food2Desk/src/app/components/order-lunch/order-lunch.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../header/header.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order-lunch',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, CommonModule, FormsModule],
   templateUrl: './order-lunch.component.html',
   styleUrl: './order-lunch.component.scss'
 })
 export class OrderLunchComponent {
+  consumptionType: 'entrega' | 'local' | null = null;
+  selectedDishIndex: number | null = null;
 
+  dailyDishes = [
+    { name: 'Parmegiana de Frango', description: 'Filé empanado com queijo e molho', price: 22.50 },
+    { name: 'Peixe Grelhado', description: 'Com legumes e arroz integral', price: 19.90 },
+    { name: 'Feijoada', description: 'Completa, com farofa e couve', price: 25.00 },
+    { name: 'Salada Caeser', description: 'Salada', price: 22.00 },
+    { name: 'Peixe a Dore', description: 'Peixe', price: 27.00 }
+  ];
+
+  selectDish(index: number) {
+    if (this.selectedDishIndex === index) {
+      this.selectedDishIndex = null;
+      this.consumptionType = null;
+    } else {
+      this.selectedDishIndex = index;
+    }
+  }
 }
