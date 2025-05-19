a2112f3354956b529152128747ac49e82038f4a7
juhgadotti <jgadotti27@gmail.com>
2025-05-19 00:54:51 -0300

    headerrrr ok

--- Food2Desk/src/app/header/header.component.ts
+++ Food2Desk/src/app/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+isUser: boolean = true;
 
+  ngOnInit(): void {
+    
+  }
 }
