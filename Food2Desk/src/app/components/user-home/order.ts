0db4418405cf689449153818da8659dbfe1b10b8
juhgadotti <jgadotti27@gmail.com>
2025-05-22 02:06:52 -0300

    isLunch no order

--- Food2Desk/src/app/components/user-home/order.ts
+++ Food2Desk/src/app/components/user-home/order.ts
@@ -6,6 +6,8 @@ export interface Order {
     totalCharge: number | null
     deliverNow: boolean
     deliveryTime: string | null
+    toDelivery: boolean
+    isLunch: boolean
     status: number
     office: { officeId: string, floor: string, number: string, block: string | null, enterprise: string | null, enterpriseId: string | null, obs: string | null} | null
     cart: Array<{id: string, quantity: number, price: number, name: string}>;
