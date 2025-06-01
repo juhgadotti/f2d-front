466fca6787f7281c31cc0ae88b24d49ec289f7b7
juhgadotti <jgadotti27@gmail.com>
2025-05-31 21:52:46 -0300

    ajustando interfaces de order e user

--- Food2Desk/src/app/components/user-home/order.ts
+++ Food2Desk/src/app/components/user-home/order.ts
@@ -7,6 +7,6 @@ export interface Order {
     toDelivery: boolean
     isLunch: boolean
     status: number
-    office: { officeId: string, floor: string, number: string, block: string | null, enterprise: string | null, enterpriseId: string | null, obs: string | null} | null
+    office: { officeId: string, floor: string, number: string, block: string | null} | null
     cart: Array<{id: string, quantity: number, price: number, name: string}>;
 }
